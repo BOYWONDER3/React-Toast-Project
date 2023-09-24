bc7073bc322ec5dea56b53e07e7735554a403582
boywonder3 <opeyemiisaacboywonder@gmail.com>
2023-09-24 03:57:00 +0100

    fourth commit

--- src/ToastProvider.jsx
+++ src/ToastProvider.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { createPortal } from "react-dom";
 
 export const ToastContext = createContext(null);
 
@@ -27,9 +28,29 @@ export function ToastProvider({ children }) {
     });
   }
 
+  const toastsByPosition = toasts.reduce((grouped, toast) => {
+    const { position } = toast.option;
+    if (grouped[position] == null) {
+      grouped[position] = [];
+    }
+    grouped[position].push(toast);
+
+    return grouped;
+  }, {});
+
   return (
     <ToastContext.Provider value={{ toast, addToast, removeToast }}>
       {children}
+      {createPortal(
+        object.enteries(toastsByPosition).map(([position, toasts]) => (
+          <div key={position} className={`toast-container ${position}`}>
+            <div className="toast">{toasts.length}</div>
+          </div>
+        )),
+        document.getElementById("toast-container")
+      )}
     </ToastContext.Provider>
   );
 }
+
+function Toast({ text }) {}
